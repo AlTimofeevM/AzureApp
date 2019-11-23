d222119bacd4ddb2d275c544d0aaac3707353cd7
AlTimofeevM <al.timofeev.m@yandex.ru>
2019-11-23 19:40:42 +0300

    Beautiful buton

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const publicPath = path.join(__dirname, '/public')
-let Chat = []
+
 
 const app = express();
 const port = process.env.PORT || 80
@@ -13,12 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(publicPath))
 
 app.get('*', (req,res) => {
-    res.render('index', {'Chat' : Chat})
-})
-
-app.post('/sendMessage', (req,res)=>{
-    Chat.push({'username': req.body.username === '' ? 'Anonymous' : req.body.username, 'message' : req.body.message})
-    res.redirect('/')
+    res.render('index')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
