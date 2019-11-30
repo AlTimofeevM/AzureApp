60b1965fa8189f76c60d1c1fd1bab857e8dfb7d3
AlTimofeevM <al.timofeev.m@yandex.ru>
2019-11-30 21:40:39 +0300

    sessions

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const publicPath = path.join(__dirname, '/public')
 const passport = require('./config/passport')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 
 const app = express();
 const port = process.env.PORT || 80
@@ -12,21 +14,44 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(publicPath))
 
-app.get('/', (req,res) => {
+app.use(
+  session({
+    secret: 'ОченьОченьСекретноеСлово',
+    store: new FileStore(),
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      maxAge: 60 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: false
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+const auth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next()
+  }
+  else {
+    return res.redirect('/auth/vkontakte')
+  }
+}
+
+app.get('/', auth, (req,res) => {
     res.render('index')
 })
 
 app.get('/auth/vkontakte',
   passport.authenticate('vkontakte'),
   function(req, res){
-    // The request will be redirected to vk.com for authentication, so
-    // this function will not be called.
   });
 
 app.get('/auth/vkontakte/callback',
-  passport.authenticate('vkontakte', { failureRedirect: '/login' }),
+  passport.authenticate('vkontakte', { failureRedirect: '/auth/vkontakte' }),
   function(req, res) {
-    // Successful authentication, redirect home.
     res.redirect('/');
   });
 
