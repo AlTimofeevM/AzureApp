55b56b41c30627175e04f9d1ba25a2a5ed0aefdc
AlTimofeevM <al.timofeev.m@yandex.ru>
2019-12-12 21:08:41 +0300

    terraform

--- terraform/terraform.js
+++ terraform/terraform.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 
-exports.init = async function(res){
-    await exec('terraform --version', (err, stdout, stderr) => {
+exports.createVM = async function(){
+    await exec('terraform apply  -auto-approve', (err, stdout, stderr) => {
         if (err) {
       
           console.error(err);
@@ -9,19 +9,7 @@ exports.init = async function(res){
           return;
         }
       
-        res.send(stdout)
+        console.log(stdout)
       });
 }
 
-exports.createVM = async function(res){
-    await exec('ansible --version', (err, stdout, stderr) => {
-        if (err) {
-      
-          console.error(err);
-      
-          return;
-        }
-      
-        res.send(stdout)
-      });     
-}
