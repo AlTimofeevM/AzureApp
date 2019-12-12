f7bbb09a421dc9896948cd8dddef8f3114fc356c
AlTimofeevM <al.timofeev.m@yandex.ru>
2019-12-12 22:34:55 +0300

    ansible

--- ansible/ansible.js
+++ ansible/ansible.js
@@ -1,6 +1,13 @@
 const Ansible = require('node-ansible');
 
-exports.createVM = function(){
+
+exports.createVM = function(){  
   var playbook = new Ansible.Playbook().playbook('createVM');
-  playbook.exec();
+  var promise = playbook.exec();
+  promise.then(function(successResult) {
+    console.log(successResult.code); // Exit code of the executed command
+    console.log(successResult.output) // Standard output/error of the executed command
+  }, function(error) {
+    console.error(error);
+  })
 }
